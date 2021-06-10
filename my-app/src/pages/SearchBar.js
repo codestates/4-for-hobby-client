bc1a05dbb76737725a9470a2d05eb865b06b0f84
dong-geon-Lee <ch9716@naver.com>
2021-06-10 10:40:01 +0900

    [client]/create-search

---
+++ my-app/src/pages/SearchBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  const [search, setSearch] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <div>
+      <form className="search__container" onSubmit={onSubmit}>
+        <input
+          className="search__text"
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="search__btn">
+          <i className="fas fa-search"></i>{" "}
+          <input type="submit" className="search__btn__input" value=""></input>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
