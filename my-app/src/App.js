bddb9dc041dcc48ae9e80bc10bfb67867b40c3ff
Kim SeungMoo <tmdan1346@gmail.com>
2021-06-10 11:54:04 +0900

    [client] backup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,63 @@
-import React from "react";
-//import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 
 import './App.css';
-import axios from 'axios';
+import axios from "axios";
+import Login from "./pages/Login"
 import Signup from "./pages/Signup";
 import Mypage from "./pages/Mypage";
 import ChattingRoom from "./pages/ChattingRoom";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userinfo, setUserinfo] = useState(null);
+
+  const isAuthenticated = () => {
+    return axios.get("https://api.cakes.com/room-list")
+      .then((res) => {
+        console.log(res)
+      })
+  }
+
+  useEffect(() => {
+    isAuthenticated();
+  }, [])
+
+  // 처음 페이지 room-list, GET / 로그인, 회원가입 LINK
+  // 로그인 후 / 로그아웃, 방생성, 회원가입 LINK
+  // 
+
   return (
     <div>
-      {/* <Mypage /> */}
-      {/* <Signup /> */}
+      {/* <Switch>
+        <Route
+          path='/login'
+          render={() => (
+            <Login isLogin={isLogin} />
+          )}
+        />
+        <Route exact path='/signup' render={() => <Signup isLogin={isLogin}/>} />
+        <Route
+          exact
+          path='/'
+          render={() => <Mypage userinfo={userinfo} />}
+        />
+        <Route
+          path='/'
+          render={() => {
+            if(isLogin) {
+              return <Redirect to='/' />;
+            }
+            return <Redirect to='/' />;
+          }}
+        />
+
+
+      </Switch> */}
       <ChattingRoom />
     </div>
   );
 }
 
+//export default withRouter(App);
 export default App;
