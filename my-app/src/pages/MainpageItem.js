49b4275bbe9eec9b45308e0ee02d3d8b27581df8
dong-geon-Lee <ch9716@naver.com>
2021-06-16 21:49:13 +0900

    [client]/create-background-css

--- my-app/src/pages/MainpageItem.js
+++ my-app/src/pages/MainpageItem.js
@@ -8,9 +8,7 @@ const MainpageItem = ({ roomInfo, deleteData, enterRoomHandler }) => {
   return roomInfo ? (
     <div>
       <div className="search__area">
-        <form
-          className="search__container"
-        >
+        <form className="search__container">
           <input
             onChange={(e) => setSearchTerm(e.target.value)}
             className="search__text"
@@ -39,6 +37,15 @@ const MainpageItem = ({ roomInfo, deleteData, enterRoomHandler }) => {
               ></DataListItem>
             ))}
         </div>
+        <section className="section__footer">
+          <h3 className="forhobby__title">
+            We are 4Forhobby! Please tell me to help!
+          </h3>
+          <h2 className="forhobby__email">Email: 4ForHobby@gmail.com</h2>
+          <p className="forhobby__rights">
+            Please join us in 2021 with 4ForHobby! - All rights reserved
+          </p>
+        </section>
       </div>
     </div>
   ) : (
